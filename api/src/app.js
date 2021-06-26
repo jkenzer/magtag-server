9b66f5228f5855f20434a9850bab8d58b4ff5ba9
jkenzer <jkenzer@gmail.com>
2021-06-26 06:29:10 -0700

    Update birthday. Add License

--- api/src/app.js
+++ api/src/app.js
@@ -1,43 +1,48 @@
-const express = require('express');
-const fetch = require('node-fetch');
-const morgan = require('morgan');
-const helmet = require('helmet');
-const cors = require('cors');
+const express = require("express");
+const fetch = require("node-fetch");
+const morgan = require("morgan");
+const helmet = require("helmet");
+const cors = require("cors");
 
-require('dotenv').config();
+require("dotenv").config();
 
-const middlewares = require('./middlewares');
+const middlewares = require("./middlewares");
 
-const SCHEDULE = require('./assets/rising-schedule-2021.json');
+const SCHEDULE = require("./assets/rising-schedule-2021.json");
 const app = express();
 
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(helmet());
 app.use(cors());
 app.use(express.json());
 
 function todayLocalTime() {
   const d = new Date();
-  const offset = (new Date().getTimezoneOffset() * 60 * 1000);
+  const offset = new Date().getTimezoneOffset() * 60 * 1000;
   const n = new Date(d.getTime() - offset);
   return n;
 }
 
 async function joke() {
-  const response = await fetch('https://icanhazdadjoke.com/', { method: 'GET', headers: { 'Accept': 'application/json' } });
+  const response = await fetch("https://icanhazdadjoke.com/", {
+    method: "GET",
+    headers: { Accept: "application/json" },
+  });
   const data = await response.json();
   return data.joke;
 }
 
 async function showerthought() {
-  const response = await fetch('https://www.reddit.com/r/Showerthoughts/hot.json');
+  const response = await fetch(
+    "https://www.reddit.com/r/Showerthoughts/hot.json"
+  );
   const data = await response.json();
   const randomPost = Math.floor(Math.random() * 25) + 1; // plus one to skip pinned posts
   return data.data.children[randomPost].data.title;
 }
 
 async function quotes() {
-  const response = await fetch('https://www.reddit.com/r/quotes/hot.json');
+  const response = await fetch("https://www.reddit.com/r/quotes/hot.json");
   const data = await response.json();
   const randomPost = Math.floor(Math.random() * 25) + 1; // plus one to skip pinned posts
   return data.data.children[randomPost].data.title;
@@ -51,38 +56,58 @@ async function cleaners() {
   if (currentDay === 4) {
     // it's thursday
     if (Math.floor((today - startDate) / (1000 * 60 * 60 * 24)) % 14 == 0) {
-      return 'Cleaners come today!';
+      return "Cleaners come today!";
     } else {
-      return 'Cleaners come next week.';
+      return "Cleaners come next week.";
     }
   } else if (currentDay < 4) {
     // thursday is 4 - current Days away
-    let nextThursday = new Date(today.getFullYear(), today.getMonth(), today.getDate() + (4 - currentDay));
-    if (Math.floor((nextThursday - startDate) / (1000 * 60 * 60 * 24)) % 14 == 0) {
-      return `Cleaners come in ${(4- currentDay)} days.`;
+    let nextThursday = new Date(
+      today.getFullYear(),
+      today.getMonth(),
+      today.getDate() + (4 - currentDay)
+    );
+    if (
+      Math.floor((nextThursday - startDate) / (1000 * 60 * 60 * 24)) % 14 ==
+      0
+    ) {
+      return `Cleaners come in ${4 - currentDay} days.`;
     } else {
-      return `Cleaners come in ${(4 - currentDay) + 7} days.`;
+      return `Cleaners come in ${4 - currentDay + 7} days.`;
     }
   } else {
     // its friday or saturday
     if (currentDay === 5) {
-      let nextThursday = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 6);
+      let nextThursday = new Date(
+        today.getFullYear(),
+        today.getMonth(),
+        today.getDate() + 6
+      );
 
-      if (Math.floor((nextThursday - startDate) / (1000 * 60 * 60 * 24)) % 14 == 0) {
-       return `Cleaners come in 6 days.`;
+      if (
+        Math.floor((nextThursday - startDate) / (1000 * 60 * 60 * 24)) % 14 ==
+        0
+      ) {
+        return `Cleaners come in 6 days.`;
       } else {
         return `Cleaners come in 13 days.`;
       }
     } else {
-      let nextThursday = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 5);
+      let nextThursday = new Date(
+        today.getFullYear(),
+        today.getMonth(),
+        today.getDate() + 5
+      );
 
-      if (Math.floor((nextThursday - startDate) / (1000 * 60 * 60 * 24)) % 14 == 0) {
+      if (
+        Math.floor((nextThursday - startDate) / (1000 * 60 * 60 * 24)) % 14 ==
+        0
+      ) {
         return `Cleaners come in 5 days.`;
       } else {
         return `Cleaners come in 12 days.`;
       }
     }
-
   }
 }
 
@@ -103,7 +128,7 @@ async function mayah() {
   return `${dateDiffFromToday(mayah)} days till Mayah's Birthday!`;
 }
 async function tyler() {
-  let tyler = new Date(2021, 05, 07);
+  let tyler = new Date(2022, 05, 07);
   return `${dateDiffFromToday(tyler)} days till Tyler's Birthday!`;
 }
 
@@ -111,7 +136,7 @@ async function rising() {
   const today = todayLocalTime();
   const schedule = SCHEDULE.reduce((accum, game) => {
     let gameDate = new Date(game.date);
-    gameDate.setUTCHours(23, 59, 0, 0)
+    gameDate.setUTCHours(23, 59, 0, 0);
     console.log(today);
     console.log(gameDate);
     if (gameDate >= today) {
@@ -122,12 +147,15 @@ async function rising() {
     }
   }, []);
 
-  let upcoming = schedule.slice(0, 3).map(game => {
+  let upcoming = schedule.slice(0, 3).map((game) => {
     if (game) {
-      let location = (game.location == "Home") ? "" : "At ";
+      let location = game.location == "Home" ? "" : "At ";
       return {
-        message: `${location}${game.opponent}, ${game.date.replace(' 2021','')} ${game.time}`
-      }
+        message: `${location}${game.opponent}, ${game.date.replace(
+          " 2021",
+          ""
+        )} ${game.time}`,
+      };
     }
   });
 
@@ -140,9 +168,7 @@ function dateDiffFromToday(futureDate) {
   return days;
 }
 
-
-app.get('/', async (req, res) => {
-
+app.get("/", async (req, res) => {
   const apps = [
     joke,
     joke,
@@ -164,19 +190,18 @@ app.get('/', async (req, res) => {
 
   const results = await apps[randomChoice]();
 
-  let message = '';
-  if (typeof (results) === 'object') {
+  let message = "";
+  if (typeof results === "object") {
     message = results[0].message;
   } else {
     message = results;
   }
   res.json({
-    message: message
+    message: message,
   });
 });
 
-app.get('/rising', async (req, res) => {
-
+app.get("/rising", async (req, res) => {
   const upcoming = await rising();
   res.json(upcoming);
 });
